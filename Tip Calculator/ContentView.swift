0edd43868914c7ee3a45535137cd123d8b955baf
Diego Gumbayan <diegogumbayan@cedarville.edu>
2026-08-21 12:59:30 -0400

    Init comit

--- Tip Calculator/ContentView.swift
+++ Tip Calculator/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tipPercentage = 0.20
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:12) {
+            Text("The Golden Fork")
+                .font(.title)
+                .bold()
+            Text("Thank you for dining with us!")
+                .font(.subheadline)
+                .foregroundStyle(.gray)
+            HStack(spacing: 12){
+                Button("15%") {tipPercentage = 0.15}
+                Button("18%") {tipPercentage = 0.18}
+                Button("20%") {tipPercentage = 0.20}
+            }
+            .buttonStyle(.bordered)
+            Divider()
+            HStack{
+                Text("Bill")
+                Spacer()
+                Text("$42.50")
+            }
+            
+            HStack{
+        Text("Tip (\(Int(tipPercentage * 100))%")
+                Spacer()
+                Text("$\(42.50 * tipPercentage, specifier: "%.2f")")
+            }
+            Divider()
+            HStack{
+                Text("$\(42.50 + 42.50 * tipPercentage, specifier: "%.2f)")")
+                    .bold()
+                Spacer()
+                Text("$51.00")
+                    .bold()
+            }
         }
         .padding()
+        .frame(width:280)
+        .background(
+            RoundedRectangle(cornerRadius: 15)
+                .fill(.white)
+                .shadow(color: .yellow, radius: 5, x: 0, y:3)
+            
+        )
+        
     }
 }
 
