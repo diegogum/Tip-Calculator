7984ca980b5f6865428e45023a47eeae351aac2f
Diego Gumbayan <diegogumbayan@cedarville.edu>
2026-08-21 12:37:01 -0400

    Initial Commit

---
+++ Tip Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tip Calculator
+//
+//  Created by Diego Gumbayan on 8/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by Diego Gumbayan on 8/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
